ea9aba29b77cdff242260648ef89fbe2e887147a
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-22 00:45:20 +0530

    Added listeners to all views. Code inside listeners to be done.

--- src/in/co/praveenkumar/scap/activities/MainActivity.java
+++ src/in/co/praveenkumar/scap/activities/MainActivity.java
@@ -4,6 +4,7 @@ import in.co.praveenkumar.scap.R;
 import in.co.praveenkumar.scap.helpers.DrawerActivity;
 import in.co.praveenkumar.scap.helpers.SuTask;
 import in.co.praveenkumar.scap.views.CustomCheckBox;
+import in.co.praveenkumar.scap.views.CustomCheckBox.OnCheckBoxStateChangeListener;
 
 import java.io.UnsupportedEncodingException;
 
@@ -20,8 +21,8 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 public class MainActivity extends DrawerActivity {
-	SeekBar frameRateSeekBar;
-	TextView frameRateTextView;
+	SeekBar bitrateSeekBar;
+	TextView bitrateTextView;
 	EditText resolutionHeightTextView;
 	EditText resolutionWidthTextView;
 	CustomCheckBox audioCheckBox;
@@ -31,26 +32,22 @@ public class MainActivity extends DrawerActivity {
 	TextView recordBtnTextView;
 	ImageView recordBtnImageView;
 
-	int bitrate;
+	int bitrate = 7;
 	String resolution;
-	int timeLimit = 30;//60 * 60;
+	int timeLimit = 30;// 60 * 60;
 	String fileName;
-
-	DisplayMetrics displayMetrics;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		setContentView(R.layout.activity_main);
 		super.onCreate(savedInstanceState);
 
-		displayMetrics = getResources().getDisplayMetrics();
 		setUpViews();
-
 	}
 
 	private void setUpViews() {
-		frameRateSeekBar = (SeekBar) findViewById(R.id.frame_rate_bar);
-		frameRateTextView = (TextView) findViewById(R.id.frame_rate_text);
+		bitrateSeekBar = (SeekBar) findViewById(R.id.frame_rate_bar);
+		bitrateTextView = (TextView) findViewById(R.id.frame_rate_text);
 		resolutionHeightTextView = (EditText) findViewById(R.id.resolution_height);
 		resolutionWidthTextView = (EditText) findViewById(R.id.resolution_width);
 		audioCheckBox = (CustomCheckBox) findViewById(R.id.audio_checkbox);
@@ -60,29 +57,69 @@ public class MainActivity extends DrawerActivity {
 		recordBtnTextView = (TextView) findViewById(R.id.record_btn_text);
 		recordBtnImageView = (ImageView) findViewById(R.id.record_btn_icon);
 
-		// Set listeners and values
-		frameRateSeekBar.setOnSeekBarChangeListener(new frameRateListener());
-		Log.d("Test", displayMetrics.heightPixels + ", "
-				+ displayMetrics.widthPixels);
+		// Set listeners
+		bitrateSeekBar.setOnSeekBarChangeListener(bitrateListener);
+		audioCheckBox.setOnCheckBoxStateChangeListener(audioListener);
+		touchCheckBox.setOnCheckBoxStateChangeListener(touchpointListener);
+		durationCheckBox.setOnCheckBoxStateChangeListener(durationListener);
+
+		// Set values
+		DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
 		resolutionHeightTextView.setText(displayMetrics.heightPixels + "");
 		resolutionWidthTextView.setText(displayMetrics.widthPixels + "");
+		bitrateSeekBar.setProgress(bitrate);
 	}
 
-	private class frameRateListener implements OnSeekBarChangeListener {
+	OnCheckBoxStateChangeListener audioListener = new OnCheckBoxStateChangeListener() {
+
+		@Override
+		public void OnCheckBoxStateChanged(boolean isChecked) {
+			// TODO Auto-generated method stub
+			Log.d("Stater", "State changed bro.");
+
+		}
+	};
+
+	OnCheckBoxStateChangeListener touchpointListener = new OnCheckBoxStateChangeListener() {
+
+		@Override
+		public void OnCheckBoxStateChanged(boolean isChecked) {
+			// TODO Auto-generated method stub
+			Log.d("Stater", "State changed bro.");
+
+		}
+	};
+
+	OnCheckBoxStateChangeListener durationListener = new OnCheckBoxStateChangeListener() {
+
+		@Override
+		public void OnCheckBoxStateChanged(boolean isChecked) {
+			// TODO Auto-generated method stub
+			Log.d("Stater", "State changed bro.");
+
+		}
+	};
+
+	OnSeekBarChangeListener bitrateListener = new OnSeekBarChangeListener() {
 
 		public void onProgressChanged(SeekBar seekBar, int progress,
 				boolean fromUser) {
 			bitrate = progress + 3;
-			frameRateTextView.setText(bitrate + "Mbps");
+			bitrateTextView.setText(bitrate + "Mbps");
 		}
 
-		public void onStartTrackingTouch(SeekBar seekBar) {
+		@Override
+		public void onStartTrackingTouch(SeekBar arg0) {
+			// TODO Auto-generated method stub
+
 		}
 
-		public void onStopTrackingTouch(SeekBar seekBar) {
-		}
+		@Override
+		public void onStopTrackingTouch(SeekBar arg0) {
+			// TODO Auto-generated method stub
 
-	}
+		}
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
